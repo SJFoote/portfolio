0c8ed97e841947d27828bead2dd62434664dbf97
Spencer Foote <83970495+SJFoote@users.noreply.github.com>
2021-09-28 14:11:55 -0700

    fade in sort of working...

--- static/js/fade.js
+++ static/js/fade.js
@@ -1,23 +1,14 @@
 $(document).ready(function() {
-    
-    /* Every time the window is scrolled ... */
     $(window).scroll( function(){
-    
-        /* Check the location of each desired element */
-        $('.hideme').each( function(i){
+        $('.fadeinleft').each( function(i){
             
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
-            /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
-                
-                $(this).animate({'opacity':'1'},500);
-                    
+            if( bottom_of_window > bottom_of_element ){
+                $(this).animate({'opacity':'1','margin-left':'0px'},1000);
             }
             
         }); 
-    
     });
-    
 });
